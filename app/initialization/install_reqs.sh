a628b26b365e63bd5579b5925528a2a4b8d0692d
jesusfberrios <jesusfberrios@gmail.com>
2024-04-04 16:40:47 -0400

    clean up install_reqs.sh

--- app/initialization/install_reqs.sh
+++ app/initialization/install_reqs.sh
@@ -8,16 +8,3 @@ sudo apt install python3-pip -y
 # Install python libraries
 pip3 install -r requirements.txt
 sudo pip3 install streamlit
-
-# EDIT THIS ACCORING YOUR DB CREDENTIALS IN YOUR VM ENV
-echo 'export DB_USERNAME="xxxxxxxx"' >> ~/.bashrc
-echo 'export DB_PASSWORD="xxxxxxxx"' >> ~/.bashrc
-echo 'export DB_HOST="xxxxxxxx"' >> ~/.bashrc
-echo 'export DB_PORT="xxxxxxxx"' >> ~/.bashrc
-echo 'export DB="xxxxxxxx"' >> ~/.bashrc
-
-# source the bash
-source ~/.bashrc
-
-# Verify that the variables were exported to bash correctly
-tail -n 10 ~/.bashrc 
